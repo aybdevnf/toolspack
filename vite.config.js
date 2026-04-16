2842164538cc4ba4ff525833db821961f4d4f67b
aybdevnf <ayoubnfinif289@gmail.com>
2026-04-16 13:42:20 +0100

    Remove allowedHosts configuration from Vite server

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    allowedHosts:[
-      "tag-cards-lasting-the.trycloudflare.com"
-    ]
-  }
+  base:'/toolspack/'
 })
